a6c388f2b9d257b502510d6166d73e1f0a59923b
ShouqAlsaleh <109864142+ShouqAlsaleh@users.noreply.github.com>
2022-08-20 13:57:18 +0300

    cw8
    الكلاس وورك قبل الاخير

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0];
 
+
+
+
 let all_grades_div = document.getElementById("all-grades");
+
+
 let search_grades_div = document.getElementById("search-grades");
 let A_grades_div = document.getElementById("A-grades");
 let bonus_grades_div = document.getElementById("bonus-grades");
@@ -8,22 +13,29 @@ let random_grade_div = document.getElementById("random-grade");
 // لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
 
 // طبّق الخطوة 4 في أسفل هذا الكومنت
-
+grades.pop();
 // طبّق الخطوة 5 في أسفل هذا الكومنت
-
+grades.push(99);
 // طبّق الخطوة 6 أسفل هذا الكومنت
-
-
+grades.forEach(all_grades_div => {
+  console.log(all_grades_div);
+  all_grades_div.innerHTML += `<div>${all_grades_div}</div>`;
+  
+});
 
 function search() {
   let inputValue = document.getElementById("input").value;
-  
+
   // طبّق الخطوة 7 أسفل هذا الكومنت
-  
+     let filteredGrades = grades.filter((x) => x == inputValue);
+
+     console.log(filteredGrades)
 }
 
 // طبّق الخطوة 8 و 9 أسفل هذا الكومنت
 
-// طبّق الخطوة 10 أسفل هذا الكومنت
+let randomIndex = Math.floor(Math.random () * grades.length);
 
+// طبّق الخطوة 10 أسفل هذا الكومنت
+random_grade_div.innerHTML = `<div>${grades[randomIndex]}</div>`;
 
